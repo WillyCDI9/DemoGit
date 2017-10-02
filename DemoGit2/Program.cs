2260de525e38699b455483823c5b1186fc94fd06
Develop2isa <develop2isa@hotmail.com>
2017-10-02 16:15:00 +0200

    Test

---
+++ DemoGit2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGit2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Personne P = new Personne("Nom", "Prénom", 53);
+            Console.WriteLine(P);
+        }
+    }
+}
