bfe497d2da0e9aecf54967d326ef57156afcef1b
Develop2isa <develop2isa@hotmail.com>
2017-10-02 14:43:36 +0200

    Déclaration des Propriétés et Constructeurs de la classe Personne

---
+++ DemoGit2/Personne.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGit2
+{
+    public class Personne
+    {
+        #region "Propriétés d'instances"
+
+        private string Nom;
+        private string Prenom;
+        private short Age;
+
+        public string _Nom
+        {
+            get { return _Nom; }
+            set { _Nom = value; }
+        }
+
+        public string _Prenom
+        {
+            get { return _Prenom; }
+            set { _Prenom = value; }
+        }
+
+        public short _Age
+        {
+            get { return _Age; }
+            set { _Age = value; }
+        }
+
+        #endregion
+
+        #region "Constructeurs"
+        public Personne (string nom, string prenom, short age)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Age = age;
+        }
+
+        #endregion
+    }
+}
